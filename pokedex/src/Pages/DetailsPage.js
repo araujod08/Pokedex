0087f114795fd1140cfc57d9919884990ed5b844
Eric Felipe <99001809+efss7@users.noreply.github.com>
2022-05-03 14:26:08 -0300

    inicio da estruturação

--- pokedex/src/Pages/DetailsPage.js
+++ pokedex/src/Pages/DetailsPage.js
@@ -1,15 +1,70 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { goBack, goToHomePage } from '../routes/Coordinator'
+import styled from 'styled-components'
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #22262c;
+
+`
+const SubHeader = styled.div`
+display: flex;
+background-color: yellow;
+flex-direction: row;
+justify-content: space-around;
+`
+
+const DetailContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+background-color: blue;
+color: white;
+`
+const FooterContainer = styled.div`
+  background-color: #0e141b;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+
+` 
+
 
 export default function DetailsPage() {
   const navigate = useNavigate()
   return (
     <div>
-      <h1>DetailsPage</h1>
+      <HeaderContainer>
+        <h1>POKEDEX</h1>
+      </HeaderContainer>
+      <SubHeader>
       <button onClick={()=>goBack(navigate)}>Voltar</button>
       <button onClick={()=>goToHomePage(navigate)}>Home</button>
-
+        </SubHeader>
+      <DetailContainer>
+      <h2>DetailContainer</h2>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+       </DetailContainer>
+       <FooterContainer>
+       <p>@2022 - Todos os direitos reservados</p>
+       </FooterContainer>
     </div>
   )
 }
--- pokedex/src/Pages/HomePage.js
+++ pokedex/src/Pages/HomePage.js
@@ -1,15 +1,70 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import {goToPokedexPage} from '../routes/Coordinator'
+import styled from 'styled-components'
 
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #22262c;
+
+`
+const SubHeader = styled.div`
+display: flex;
+background-color: yellow;
+flex-direction: column;
+align-items: center;
+`
+
+const HomeContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+background-color: blue;
+color: white;
+`
+const FooterContainer = styled.div`
+  background-color: #0e141b;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+` 
 
 
 export default function HomePage() {
   const navigate = useNavigate()
   return (
-    <div>
-      <h1>HomePage</h1>
-       <button onClick={()=>goToPokedexPage(navigate)}>Pokedex</button>
+      <div>
+      <HeaderContainer>
+        <h1>POKEDEX</h1>
+        
+      </HeaderContainer>
+      <SubHeader>
+          <button onClick={()=>goToPokedexPage(navigate)}>Pokedex</button>
+        </SubHeader>
+      <HomeContainer>
+      <h2>HomePage</h2>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+      <p>CONTEUDO</p>
+       </HomeContainer>
+       <FooterContainer>
+       <p>@2022 - Todos os direitos reservados</p>
+       </FooterContainer>
     </div>
   )
 }

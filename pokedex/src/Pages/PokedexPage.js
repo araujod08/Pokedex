ae56ecb5844300b15c84c64b53bfe96a18a76eac
Araujod12 <davia9a@gmail.com>
2022-05-04 18:40:58 -0300

    estilizacões card

--- pokedex/src/Pages/PokedexPage.js
+++ pokedex/src/Pages/PokedexPage.js
@@ -2,13 +2,20 @@ import React from 'react'
 import { goBack, goToDetailPage } from '../routes/Coordinator'
 import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
+import Imagem from '../img/Header2.png'
 
 const HeaderContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   background-color: #22262c;
-
+  display: flex;
+flex-direction: column;
+align-items: center;
+background-color: #22262c;
+img{
+width: 60vw;
+}
 `
 const SubHeader = styled.div`
 display: flex;
@@ -30,40 +37,25 @@ const FooterContainer = styled.div`
   align-items: center;
   justify-content: center;
   color: white;
-` 
+`
 
 
 export default function PokedexPage() {
- const navigate = useNavigate()
+  const navigate = useNavigate()
   return (
     <div>
       <HeaderContainer>
-        <h1>POKEDEX</h1>
+        <img src={Imagem} />
       </HeaderContainer>
       <SubHeader>
-      <button onClick={()=>goBack(navigate)}>Voltar</button>
-      <button onClick={()=>goToDetailPage(navigate)}>Detalhes</button>
-        </SubHeader>
+        <button onClick={() => goBack(navigate)}>Voltar</button>
+        <button onClick={() => goToDetailPage(navigate)}>Detalhes</button>
+      </SubHeader>
       <PokedexContainer>
-      <h2>PokedexPage</h2>
-      <p>CONTEUDO</p>
-      <p>CONTEUDO</p>
-      <p>CONTEUDO</p>
-      <p>CONTEUDO</p>
-      <p>CONTEUDO</p>
-      <p>CONTEUDO</p>
-      <p>CONTEUDO</p>
-      <p>CONTEUDO</p>
-      <p>CONTEUDO</p>
-      <p>CONTEUDO</p>
-      <p>CONTEUDO</p>
-      <p>CONTEUDO</p>
-      <p>CONTEUDO</p>
-      <p>CONTEUDO</p>
-       </PokedexContainer>
-       <FooterContainer>
-       <p>@2022 - Todos os direitos reservados</p>
-       </FooterContainer>
+      </PokedexContainer>
+      <FooterContainer>
+        <p>@2022 - Todos os direitos reservados</p>
+      </FooterContainer>
     </div>
   )
 }
